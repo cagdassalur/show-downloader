14bb739df1938f5d358dad53fd5eecc4937f3ef2
cagdassalur <cagdas544@gmail.com>
2014-03-31 02:34:25 +0300

    v0.1

--- dizi.py
+++ dizi.py
@@ -8,14 +8,15 @@ f=open('dizi.txt','r')
 st = f.read()
 f.close()
 #with open('output.txt', 'w') as f: f.write(st)
-
-for line in st.split('\n'):
+ar = st.split('\n')
+st_out = ''
+for line in ar:
 	line = line.split(';')
 	isim = line[0].replace(' ', '%20')
 	ep = int(line[1])
 	url = "http://194.71.107.80/search/{0}/0/7/0".format(isim)
 	reg = re.compile(r"([sS]\d+[eE]\d+)")
-	inenler = []
+	inenler = [ep]
 
 	soup = BeautifulSoup(br.open(url).read())
 	isim = isim.replace('%20',' ')
@@ -26,6 +27,13 @@ for line in st.split('\n'):
 			epn = int(found.group()[1:].lower().replace('e', ''))
 			if epn > ep:
 				if( (epn not in inenler) and
-					raw_input(isim + '/' + str(epn) + ' bulundu. indir? [y-n]').lower() == 'y'):
-					os.startfile(magnet)
+					#raw_input(isim + '/' + str(epn) + ' bulundu. indir? [y-n]').lower() == 'y'):
+					True:
+					os.system("open "+magnet)
 					inenler.append(epn)
+	
+	for i in xrange(len(ar)):
+		if isim in ar[i]: 
+			ar[i] = isim + ';' + str(max(inenler))
+with open('dizi.txt','w') as f: f.write('\n'.join(ar))
+
